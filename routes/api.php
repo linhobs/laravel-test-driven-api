e697a618b9f761fddbf9d9ac8d10fb3f6861fadf
Pius <p.gyamenah@nsano.com>
2021-12-05 04:48:39 +0000

    added feature test for fetch todoLists

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 
 Route::get('todo-list',[TodoListController::class,'index'])->name('todo-list.store');
+Route::get('todo-list/{id}',[TodoListController::class,'show'])->name('todo-list.show');
+
